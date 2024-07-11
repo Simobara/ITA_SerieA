b26dfb64bb58872ab2e3cc72dff201e8fb3ace8f
M <0ilbaroneutente@M.local>
2024-07-11 23:12:24 +0200

    update loginPage

--- src/LoginPage/loginPageNormal.jsx
+++ src/LoginPage/loginPageNormal.jsx
@@ -1,5 +1,7 @@
 // ? PAGINA LOGIN NORMALE
 import React, { useState } from 'react';
+import img1 from '../../imgFratello.png';
+
 
 const LoginPage = ({ onLogin }) => {
   const [username, setUsername] = useState('');
@@ -15,8 +17,8 @@ const LoginPage = ({ onLogin }) => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <form className="bg-white p-6 rounded-lg shadow-md w-full max-w-sm" onSubmit={handleSubmit}>
+    <div className="flex justify-center items-center h-screen bg-cover bg-center" style={{ backgroundImage: `url(${img1})` }}>
+      <form className="bg-white bg-opacity-80 p-6 rounded-lg shadow-md w-full max-w-sm mt-[42rem]" onSubmit={handleSubmit}>
         <h2 className="text-2xl font-semibold mb-6 text-center">Login</h2>
         <input 
           type="text" 
@@ -32,7 +34,7 @@ const LoginPage = ({ onLogin }) => {
           onChange={(e) => setPassword(e.target.value)} 
           className="w-full p-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200">
+        <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200 ">
           Login
         </button>
       </form>
