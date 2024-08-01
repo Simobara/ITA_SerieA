8128631c2aac64b950766f30bf59f96eb1ba5b8c
SimonBara <simobara@hotmail.it>
2024-08-01 18:41:28 +0200

    bar coppaIta

--- src/Body/PagCoppaIta/Colonne/colonne.jsx
+++ src/Body/PagCoppaIta/Colonne/colonne.jsx
@@ -1,22 +1,22 @@
-import ATrentaduesimi 	from './Disposiz/1Trentaduesimi/ATrentaduesimi';
-import BTrentaduesimi 	from './Disposiz/1Trentaduesimi/BTrentaduesimi';
-import ASedicesimi 		from './Disposiz/2Sedicesimi/ASedicesimi';
-import BSedicesimi 		from './Disposiz/2Sedicesimi/BSedicesimi';
-import AOttavi 			from './Disposiz/3Ottavi/AOttavi';
-import BOttavi 			from './Disposiz/3Ottavi/BOttavi';
-import AQuarti 			from './Disposiz/4Quarti/AQuarti';
-import BQuarti 			from './Disposiz/4Quarti/BQuarti';
-import ASemifinali 		from './Disposiz/5Semifinali/ASemifinali';
-import BSemifinali 		from './Disposiz/5Semifinali/BSemifinali';
-import Finale 			from './Disposiz/6Finale/6Finale';
+import ATrentaduesimi from './Disposiz/1Trentaduesimi/ATrentaduesimi';
+import BTrentaduesimi from './Disposiz/1Trentaduesimi/BTrentaduesimi';
+import ASedicesimi from './Disposiz/2Sedicesimi/ASedicesimi';
+import BSedicesimi from './Disposiz/2Sedicesimi/BSedicesimi';
+import AOttavi from './Disposiz/3Ottavi/AOttavi';
+import BOttavi from './Disposiz/3Ottavi/BOttavi';
+import AQuarti from './Disposiz/4Quarti/AQuarti';
+import BQuarti from './Disposiz/4Quarti/BQuarti';
+import ASemifinali from './Disposiz/5Semifinali/ASemifinali';
+import BSemifinali from './Disposiz/5Semifinali/BSemifinali';
+import Finale from './Disposiz/6Finale/6Finale';
 
 
 const Colonne = ({ colonneTitle, colonneIdN }) => {
 
 	return (
 		<div className="relative col-span-1 bg-transparent text-gray-500 font-bold text-sm flex flex-col h-full w-[110px]">
-			<div className="mt-2 mb-2 text-center">{colonneTitle}</div>
-			<div className="flex flex-col items-start justify-start flex-grow">
+			<div className="mt-2 mb-2 text-center z-20 pt-[1rem]">{colonneTitle}</div>
+			<div className="flex flex-col items-start justify-start flex-grow ">
 				{(colonneTitle === 'TRENTADUESIMI' 		&& colonneIdN ===  '1') &&	<ATrentaduesimi 	/>}
 				{(colonneTitle === 'SEDICESIMI' 		&& colonneIdN ===  '2') &&	<ASedicesimi 		/>}
 				{(colonneTitle === 'OTTAVI' 			&& colonneIdN ===  '3') && 	<AOttavi 			/>}
