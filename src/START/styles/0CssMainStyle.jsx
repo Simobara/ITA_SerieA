5aee46404f5fcf93cf1cf3408cd4f923e5695668
SimonBara <simobara@hotmail.it>
2024-08-02 00:25:08 +0200

    refactoring

--- src/START/styles/0CssMainStyle.jsx
+++ src/START/styles/0CssMainStyle.jsx
@@ -1,18 +1,18 @@
 //sezione classifica - sezione1
 export const s = {
-  Bg0: "bg-transparent",
-  Bg1: "bg-gray-700",
-  Bg2: "bg-gray-700/70", // colore per hover qunado seleziono squadre
-  Bg3: "bg-gray-800/70",
+  Bg0:              "bg-transparent",
+  Bg1:              "bg-gray-700",
+  Bg2:              "bg-gray-700/70", // colore per hover qunado seleziono squadre
+  Bg3:              "bg-gray-800/70",
 
-  Filter: "filter brightness-[50%]",
-  Filter1: "filter brightness-[80%]",
-  Filter2: "filter brightness-[90%]", //no mod
-  Filter3: "filter brightness-[90%]", //colonna PTS
-  Filter4: "filter brightness-[80%]", //colonna SQ - se ha gia' giocato
+  Filter:           "filter brightness-[50%]",
+  Filter1:          "filter brightness-[80%]",
+  Filter2:          "filter brightness-[90%]", //no mod
+  Filter3:          "filter brightness-[90%]", //colonna PTS
+  Filter4:          "filter brightness-[80%]", //colonna SQ - se ha gia' giocato
 
-  BaseText: "text-gray-700",
-  BaseText1: "md:text-md font-bold",
+  BaseText:         "text-gray-700",
+  BaseText1:        "md:text-md font-bold",
 
   ImgTextInRoundMd: "text-md",
   ImgTextInRoundSm: "text-sm",
@@ -20,36 +20,35 @@ export const s = {
 
 //sezione classifica - sezione3
 export const ts = {
-  Bg0: "bg-transparent",
-  Bg9: "bg-black",
+  Bg0:          "bg-transparent",
+  Bg9:          "bg-black",
 
-  TextCF: "text-sky-500",
+  TextCF:       "text-sky-500",
   //--- --- --- --- --- --- --- --- --- --- --- --- ---
-  ATeamBg:
-    "bg-sky-900 border-t-2 border-l-4 border-cyan-400 rounded-t-sm rounded-l-3xl rounded-tr-xl",
-  ATeamText: "text-black tracking-wider font-black",
+  ATeamBg:      "bg-sky-900 border-t-2 border-l-4 border-cyan-400 rounded-t-sm rounded-l-3xl rounded-tr-xl",
+  ATeamText:    "text-black tracking-wider font-black",
 
-  BTeamText: "text-gray-600",
+  BTeamText:    "text-gray-600",
 
-  ABTeamText: "text-sky-800 font-bold",
+  ABTeamText:   "text-sky-800 font-bold",
   //--- --- --- --- --- --- --- --- --- --- --- --- --- ---
-  ColResBg: "!text-gray-800",
-  ColResLine: "text-gray-500",
-  ColResHome: "!text-pink-500 font-semibold",
-  ColResAway: "text-sky-800 font-light",
+  ColResBg:     "!text-gray-800",
+  ColResLine:   "text-gray-500",
+  ColResHome:   "!text-pink-500 font-semibold",
+  ColResAway:   "text-sky-800 font-light",
 
   TabHoverHome: "hover:bg-gray-300",
   TabHoverAway: "hover:bg-gray-800",
 
-  WinBg: "bg-green-600 border-b-2 border-green-950",
-  WinText: "!text-green-600",
+  WinBg:        "bg-green-600 border-b-2 border-green-950",
+  WinText:      "!text-green-600",
 
-  DrawBg: "bg-sky-900 border-b-2 border-sky-950",
-  DrawText: "!text-sky-900",
+  DrawBg:       "bg-sky-900 border-b-2 border-sky-950",
+  DrawText:     "!text-sky-900",
 
-  LoseBg: "border-2 border-gray-700 bg-black",
-  LoseText: "!text-gray-700/20",
+  LoseBg:       "border-2 border-gray-700 bg-black",
+  LoseText:     "!text-gray-700/20",
 
-  HoverSqHome: "bg-red-700",
-  HoverSqAway: "bg-sky-800",
+  HoverSqHome:  "bg-red-700",
+  HoverSqAway:  "bg-sky-800",
 };
