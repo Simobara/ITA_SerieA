2281ef2f4ff0711f168bd84ab03d34f1bbaede35
SimonBara <simobara@hotmail.it>
2024-08-05 23:51:05 +0200

    refactor w size

--- src/Body/PagSerieA/pagSerieA.jsx
+++ src/Body/PagSerieA/pagSerieA.jsx
@@ -14,17 +14,17 @@ const PagSerieA = () => {
       {/* <div className="w-6 bg-black"></div> */}
       {/* </div> */}
       <div className="overflow-hidden lg:w-[1299px] flex flex-wrap md:flex-wrap lg:flex-nowrap">
-        <div className="overflow-y-scroll sm:overflow-y-auto min-w-[90vw] min-h-full sm:min-w-[17rem] sm:max-h-[50vh] md:min-w-[17rem] md:min-h-full lg:min-w-[18rem] lg:min-h-[95vh] xl:max-w-[100%] xl:max-h-[95vh] max-w-[15rem] max-h-[50vh]">
+        <div className="overflow-y-scroll sm:overflow-y-auto min-w-[98vw] min-h-full sm:min-w-[17rem] sm:max-h-[50vh] md:min-w-[17rem] md:min-h-full lg:min-w-[18rem] lg:min-h-[95vh] xl:max-w-[100%] xl:max-h-[95vh] max-w-[15rem] max-h-[50vh]">
           <div className="border border-r-0 rounded-[20px] border-gray-900 bg-black unselectable">
             <TableClass />
           </div>
         </div>
-        <div className="overflow-y-scroll sm:overflow-y-auto min-w-[90vw] sm:min-w-[22.5rem] sm:max-h-[50vh]  md:max-w-[30.15rem] md:max-h-[50vh] lg:min-w-[30rem] lg:min-h-[95vh] xl:max-w-[100%] xl:max-h-[95vh] max-w-[15rem] max-h-[40vh]">
+        <div className="overflow-y-scroll sm:overflow-y-auto min-w-[98vw] sm:min-w-[22.5rem] sm:max-h-[50vh]  md:max-w-[30.15rem] md:max-h-[50vh] lg:min-w-[30rem] lg:min-h-[95vh] xl:max-w-[100%] xl:max-h-[95vh] max-w-[15rem] max-h-[40vh]">
           <div className="border-4 border-l-0 border-r-0 rounded-[20px] border-gray-900 bg-black">
             <TableProxInc />
           </div>
         </div>
-        <div className="overflow-y-scroll sm:overflow-x-hidden sm:overflow-y-auto min-w-[90vw] sm:min-w-full sm:max-h-[50vh] md:min-w-[76.9vh] md:max-h-[50vh] lg:min-w-[30rem] lg:min-h-[95vh] lg:w-auto xl:max-w-[100%] xl:max-h-[95vh] max-w-[15rem] max-h-[50vh]">
+        <div className="overflow-y-scroll sm:overflow-x-hidden sm:overflow-y-auto min-w-[98vw] sm:min-w-full sm:max-h-[50vh] md:min-w-[76.9vh] md:max-h-[50vh] lg:min-w-[30rem] lg:min-h-[95vh] lg:w-auto xl:max-w-[100%] xl:max-h-[95vh] max-w-[15rem] max-h-[50vh]">
           <div className="border border-l-0 rounded-[20px] border-gray-900 bg-black unselectable">
             <LogoSquadrePos />
           </div>
