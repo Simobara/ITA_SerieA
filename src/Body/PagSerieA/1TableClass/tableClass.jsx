016ee2fd20d64b568f840677e119026a67db85dd
SimonBara <simobara@hotmail.it>
2024-08-06 11:56:38 +0200

    refactoring w TableClass

--- src/Body/PagSerieA/1TableClass/tableClass.jsx
+++ src/Body/PagSerieA/1TableClass/tableClass.jsx
@@ -276,12 +276,10 @@ const TableClass = () => {
             <table className="relative overflow-x-hidden overflow-y-hidden md:min-h-[57rem] max-w-[46%]">
               <thead>
                 <tr className="bg-black text-gray-600 text-center">
-                  <th className="w-[0.2rem] sm:w-[0.5rem]" style={{ whiteSpace: "nowrap" }}>
-                    --
-                  </th>
+                  <th className="w-[0.2rem] sm:w-[0.5rem]" style={{ whiteSpace: "nowrap" }}></th>
                   <th className="w-[20%] sm:pl-[1rem]">----- SQUADRA</th>
                   <th className="w-[1rem] sm:pl-[1rem] pl-[0.5rem]">?</th>
-                  <th className="w-[1rem] sm:pl-[1rem] pl-[0.5rem] text-center">PTS</th>
+                  <th className="w-[1rem] sm:pl-[1rem] pl-[0.5rem] text-center">Ps</th>
                 </tr>
               </thead>
               <tbody className="bg-black text-cyan-800">
@@ -343,12 +341,10 @@ const TableClass = () => {
             <table className="relative overflow-x-hidden overflow-y-hidden md:min-h-[57rem] max-w-[46%] ml-[0rem]">
               <thead>
                 <tr className="bg-black text-gray-600 text-center">
-                  <th className="w-[1rem] sm:w-[0.5rem]" style={{ whiteSpace: "nowrap" }}>
-                    --
-                  </th>
+                  <th className="w-[1rem] sm:w-[0.5rem]" style={{ whiteSpace: "nowrap" }}></th>
                   <th className="w-[20%] sm:pl-[1rem] "> ----- SQUADRA </th>
                   <th className="w-[1rem] sm:pl-[1rem] pl-[0.5rem]">?</th>
-                  <th className="w-[1rem] sm:pl-[1rem] pl-[0.5rem] text-center">PTy</th>
+                  <th className="w-[1rem] sm:pl-[1rem] pl-[0.5rem] text-center">Ps</th>
                 </tr>
               </thead>
               <tbody className="bg-black text-cyan-800">
