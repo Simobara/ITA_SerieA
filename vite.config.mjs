cb07a195020d2df6b04b8487fe0bf74fadab61f7
SimonBara <simobara@hotmail.it>
2024-08-12 20:05:37 +0200

    environment variables

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://ita-serie-a.vercel.app", // URL della tua API in produzione
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
 });
