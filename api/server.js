27b46c7752b1850e75025edb51ec7c21c1cdf35f
SimonBara <simobara@hotmail.it>
2024-08-17 16:11:40 +0200

    deploy verify .env

--- api/server.js
+++ api/server.js
@@ -7,6 +7,19 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.NODE_ENV === 'production' ? 3000 : 5000;
 
+
+// Log per verificare che le variabili d'ambiente siano correttamente lette
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("USERNAME:", process.env.USERNAME);
+console.log("PASSWORD:", process.env.PASSWORD ? "*****" : "Mancante"); // Non loggare direttamente la password
+console.log("CLUSTER_URL:", process.env.CLUSTER_URL);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("VITE_API_URL_PRODUCTION:", process.env.VITE_API_URL_PRODUCTION);
+console.log("VITE_API_URL_DEVELOPMENT:", process.env.VITE_API_URL_DEVELOPMENT);
+
+
+
+
 // Middleware CORS
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production'
