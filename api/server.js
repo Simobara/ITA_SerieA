46880efc4377996849f6768ba57963acb4c5f9b0
SimonBara <simobara@hotmail.it>
2024-08-18 00:20:37 +0200

    overall styles

--- api/server.js
+++ api/server.js
@@ -8,13 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 console.log("----------------------------------");
-console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("USERNAME:", process.env.USERNAME);
-console.log("PASSWORD:", process.env.PASSWORD ? "*****" : "Mancante");
+console.log("NODE_ENV:",    process.env.NODE_ENV);
+console.log("USERNAME:",    process.env.USERNAME);
+console.log("PASSWORD:",    process.env.PASSWORD ? "*****" : "Mancante");
 console.log("CLUSTER_URL:", process.env.CLUSTER_URL);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("VITE_API_URL_PRODUCTION:", process.env.VITE_API_URL_PRODUCTION);
-console.log("VITE_API_URL_DEVELOPMENT:", process.env.VITE_API_URL_DEVELOPMENT);
+console.log("DB_NAME:",     process.env.DB_NAME);
+console.log("VITE_API_URL_PRODUCTION:",   process.env.VITE_API_URL_PRODUCTION);
+console.log("VITE_API_URL_DEVELOPMENT:",  process.env.VITE_API_URL_DEVELOPMENT);
 console.log("----------------------------------");
 
 // Middleware CORS
@@ -45,14 +45,12 @@ async function connectToDatabase() {
   try {
     console.log("Trying to connect to MongoDB...");
     const connection = await mongoose.connect(dbURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000, // Timeout dopo 5 secondi
       socketTimeoutMS: 45000,         // Chiudi i socket dopo 45 secondi di inattività
       family: 4                       // Usa IPv4, ignora IPv6
     });
 
-    cachedDb = connection; // Memorizza la connessione nel cache
+    cachedDb = connection;            // Memorizza la connessione nel cache
     console.log("Connected to MongoDB Atlas in dev/prod environment");
 
     return cachedDb;
@@ -95,7 +93,6 @@ app.get('/api/test', (req, res) => {
   res.status(200).send('Test endpoint is working!');
 });
 
-// Conferma che il server è in esecuzione
 app.listen(PORT, () => {
   console.log(`Server.js => Server running on port ${PORT}`);
 });
