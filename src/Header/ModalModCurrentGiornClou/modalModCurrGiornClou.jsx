63cefdab7a557bda0d721841dfa6694d70420a11
SimonBara <simobara@hotmail.it>
2024-08-18 01:23:01 +0200

    header modalCurrGiornClou

--- src/Header/ModalModCurrentGiornClou/modalModCurrGiornClou.jsx
+++ src/Header/ModalModCurrentGiornClou/modalModCurrGiornClou.jsx
@@ -96,7 +96,7 @@ const ModalModCurrGiornClou = ({ onClose, onSave }) => {
   return (
     <>
       <div className="fixed inset-0 flex items-start justify-start z-50 bg-gray-900 bg-opacity-50">
-        <div className="relative w-[700px] h-[650px] shadow-xxxl rounded-lg border-4 border-sky-900 bg-black overflow-auto ml-[2rem]">
+        <div className="relative w-[700px] h-[650px] shadow-xxxl rounded-lg border-4 border-sky-900 bg-black overflow-auto ml-[-2rem] mr-[0]">
           <div className="absolute top-0 left-0 right-0 bg-gray-950 z-20">
             <button className="text-3xl leading-none text-sky-700 w-full hover:bg-sky-800 hover:text-white" onClick={onClose}>
               X
