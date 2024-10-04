88e9eb3ecf802353abf86927524cf6fa089b4419
SimonBara <simobara@hotmail.it>
2024-10-04 15:15:05 +0200

    pron 7gg

--- src/START/app/0aCalendarioCurrent.jsx
+++ src/START/app/0aCalendarioCurrent.jsx
@@ -98,14 +98,14 @@ export const calendario25 = {
       { numero: 2,  day: 'ven', time: '20:45',          team1: 'Verona',       team2: 'Venezia',         pron: '', results: '' },
 
       { numero: 3,  day: 'sab', time: '15:00',          team1: 'Udinese',      team2: 'Lecce',           pron: '1', results: '' },
-      { numero: 4,  day: 'sab', time: '18:00',          team1: 'Atalanta',     team2: 'Genoa',           pron: '', results: '' },
-      { numero: 5,  day: 'sab', time: '20:45',          team1: 'Inter',        team2: 'Torino',          pron: '', results: '' },
+      { numero: 4,  day: 'sab', time: '18:00',          team1: 'Atalanta',     team2: 'Genoa',           pron: 'X', results: '' },
+      { numero: 5,  day: 'sab', time: '20:45',          team1: 'Inter',        team2: 'Torino',          pron: '1', results: '' },
       
       { numero: 6,  day: 'dom', time: '12:30',          team1: 'Juve',         team2: 'Cagliari',        pron: '1', results: '' },
-      { numero: 7,  day: 'dom', time: '15:00',          team1: 'Bologna',      team2: 'Parma',           pron: '', results: '' },
+      { numero: 7,  day: 'dom', time: '15:00',          team1: 'Bologna',      team2: 'Parma',           pron: '1', results: '' },
       { numero: 8,  day: 'dom', time: '15:00',          team1: 'Lazio',        team2: 'Empoli',          pron: '1', results: '' },
-      { numero: 9,  day: 'dom', time: '18:00',          team1: 'Monza',        team2: 'Roma',            pron: '', results: '' },
-      { numero: 10, day: 'dom', time: '20:45',          team1: 'Fiorentina',   team2: 'Milan',           pron: '', results: '' },  
+      { numero: 9,  day: 'dom', time: '18:00',          team1: 'Monza',        team2: 'Roma',            pron: '2', results: '' },
+      { numero: 10, day: 'dom', time: '20:45',          team1: 'Fiorentina',   team2: 'Milan',           pron: 'X', results: '' },  
   ],
     //*---  -----                                                                                                                ---  8
       giornata8: [
