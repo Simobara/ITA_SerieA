e56a6a242269c302f0cd17919cc820bd9a25cb5e
SimonBara <simobara@hotmail.it>
2024-10-10 12:45:06 +0200

    pron 8gg

--- src/START/app/0aCalendarioCurrent.jsx
+++ src/START/app/0aCalendarioCurrent.jsx
@@ -114,13 +114,13 @@ export const calendario25 = {
       { numero: 3,  day: 'sab', time: '18:00',          team1: 'Milan',        team2: 'Udinese',         pron: '1', results: '' },
       { numero: 4,  day: 'sab', time: '20:45',          team1: 'Juve',          team2: 'Lazio',           pron: '2', results: '' },
 
-      { numero: 5,  day: 'dom', time: '12:30',          team1: 'Empoli',       team2: 'Napoli',          pron: 'X', results: '' },
+      { numero: 5,  day: 'dom', time: '12:30',          team1: 'Empoli',       team2: 'Napoli',          pron: '1', results: '' },
       { numero: 6,  day: 'dom', time: '15:00',          team1: 'Lecce',        team2: 'Fiorentina',      pron: '1', results: '' },
       { numero: 7,  day: 'dom', time: '15:00',          team1: 'Venezia',      team2: 'Atalanta',        pron: 'X', results: '' },      
       { numero: 8,  day: 'dom', time: '18:00',          team1: 'Cagliari',     team2: 'Torino',          pron: '1', results: '' },
-      { numero: 9,  day: 'dom', time: '20:45 ',          team1: 'Roma',         team2: 'Inter',           pron: '2', results: '' },
+      { numero: 9,  day: 'dom', time: '20:45 ',          team1: 'Roma',         team2: 'Inter',           pron: 'X', results: '' },
 
-      { numero: 10, day: 'lun', time: '20:45',          team1: 'Verona',       team2: 'Monza',           pron: 'X', results: '' },
+      { numero: 10, day: 'lun', time: '20:45',          team1: 'Verona',       team2: 'Monza',           pron: '2', results: '' },
   
   ],
     //*--------------------------------------------------------    -  9 -    -------------------------------------------------------  9
