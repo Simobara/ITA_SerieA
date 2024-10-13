9c22e2c95829168321ade9648a0c2d3c235c2314
SimonBara <simobara@hotmail.it>
2024-10-13 13:56:03 +0200

    pron 8gg

--- src/START/app/0aCalendarioCurrent.jsx
+++ src/START/app/0aCalendarioCurrent.jsx
@@ -115,9 +115,9 @@ export const calendario25 = {
       { numero: 4,  day: 'sab', time: '20:45',          team1: 'Juve',         team2: 'Lazio',           pron: '2', results: '' },
 
       { numero: 5,  day: 'dom', time: '12:30',          team1: 'Empoli',       team2: 'Napoli',          pron: 'X', results: '' },
-      { numero: 6,  day: 'dom', time: '15:00',          team1: 'Lecce',        team2: 'Fiorentina',      pron: '1', results: '' },
+      { numero: 6,  day: 'dom', time: '15:00',          team1: 'Lecce',        team2: 'Fiorentina',      pron: 'X', results: '' },
       { numero: 7,  day: 'dom', time: '15:00',          team1: 'Venezia',      team2: 'Atalanta',        pron: '2', results: '' },      
-      { numero: 8,  day: 'dom', time: '18:00',          team1: 'Cagliari',     team2: 'Torino',          pron: 'X', results: '' },
+      { numero: 8,  day: 'dom', time: '18:00',          team1: 'Cagliari',     team2: 'Torino',          pron: '1', results: '' },
       { numero: 9,  day: 'dom', time: '20:45 ',         team1: 'Roma',         team2: 'Inter',           pron: '2', results: '' },
 
       { numero: 10, day: 'lun', time: '20:45',          team1: 'Verona',       team2: 'Monza',           pron: '2', results: '' },
