41295d2863d1f58a2d1f558f09553d324bb6c0bf
SimonBara <simobara@hotmail.it>
2024-10-15 19:14:23 +0200

    reduction my side2

--- src/Body/PagSerieA/2TableProxInc/Partite/Day/Partita/partita.jsx
+++ src/Body/PagSerieA/2TableProxInc/Partite/Day/Partita/partita.jsx
@@ -351,7 +351,7 @@ const Partita = ({ partita, resetAll, occhioApertoPartita, setOcchioApertoPartit
   return (
     <>
       <div
-        className={`font-bold flex items-center justify-center my-[-0.3rem] sm:my-[0rem]
+        className={`font-bold flex items-center justify-center my-[-0.25rem] sm:my-[0rem]
                 ${isPartitaModificabile ? "" : "unselectable"}`}
       >
         <div className="flex items-center justify-center xs:text-xs sm:text-sm relative">
