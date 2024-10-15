10bc4086cb37cf337ee704313d4fab2e9fd6e5a9
SimonBara <simobara@hotmail.it>
2024-10-15 18:34:31 +0200

    mobile view

--- src/Body/PagSerieA/1TableClass/tableClass.jsx
+++ src/Body/PagSerieA/1TableClass/tableClass.jsx
@@ -255,7 +255,7 @@ const TableClass = () => {
         </table>
       )}
       {isMobile && (
-        <div className="table-container max-h-[40.5vh]">
+        <div className="table-container max-h-[42.5vh]">
           <div className="table-column">
             <table className="relative overflow-x-hidden overflow-y-hidden md:min-h-[57rem] max-w-[50%] ml-[0]">
               <thead style={{ visibility: "hidden" }}>
