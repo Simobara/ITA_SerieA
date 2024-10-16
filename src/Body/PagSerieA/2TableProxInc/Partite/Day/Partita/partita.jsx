c0d9433a40c442e1394c820c8f44d311c2bd34fc
SimonBara <simobara@hotmail.it>
2024-10-16 03:37:18 +0200

    movement symbol part2

--- src/Body/PagSerieA/2TableProxInc/Partite/Day/Partita/partita.jsx
+++ src/Body/PagSerieA/2TableProxInc/Partite/Day/Partita/partita.jsx
@@ -351,7 +351,7 @@ const Partita = ({ partita, resetAll, occhioApertoPartita, setOcchioApertoPartit
   return (
     <>
       <div
-        className={`font-bold flex items-center justify-center my-[0.08rem]
+        className={`font-bold flex items-center justify-center my-[-0.1rem]
                 ${isPartitaModificabile ? "" : "unselectable"}`}
       >
         <div className="flex items-center justify-center xs:text-xs sm:text-sm relative">
@@ -368,11 +368,12 @@ const Partita = ({ partita, resetAll, occhioApertoPartita, setOcchioApertoPartit
                         <div className="text-xl font-bold"></div>
                     </div> */}
           {!isPartitaInCoppiaRegSelected && (
-            <div className={`{sm:relative absolute flex ml-[33rem] sm:pl-0 hover:cursor-context-menu z-10 select-none }`}>
+            <div className={`{sm:relative absolute flex ml-[5rem] sm:pl-0 hover:cursor-context-menu z-10 select-none }`}>
               <div className={`{sm:pr-1 pl-3 ${!isPartitaInCoppiaRegSelected} ? ml-[-0.5rem] mr-[-2rem] : 'pl-4'}`}>
                 <span className="text-lg hover:bg-sky-900 text-gray-700/70 z-20" role="img" aria-label="" onClick={() => handleResetColors(partita.team1, partita.team2)}>
                   {/* ,,, */}
-                  {/* 〰️ */}~
+                  {/* 〰️ */}
+                  ~~
                 </span>
               </div>
             </div>
