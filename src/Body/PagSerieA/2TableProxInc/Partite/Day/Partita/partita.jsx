b3ddf3c4e058de71dbd26716dd380cc36b658bcd
SimonBara <simobara@hotmail.it>
2024-10-16 03:05:27 +0200

    movement symbol part2

--- src/Body/PagSerieA/2TableProxInc/Partite/Day/Partita/partita.jsx
+++ src/Body/PagSerieA/2TableProxInc/Partite/Day/Partita/partita.jsx
@@ -368,9 +368,9 @@ const Partita = ({ partita, resetAll, occhioApertoPartita, setOcchioApertoPartit
                         <div className="text-xl font-bold"></div>
                     </div> */}
           {!isPartitaInCoppiaRegSelected && (
-            <div className={`{sm:relative absolute flex ml-[7rem] sm:pl-0 hover:cursor-context-menu z-10 select-none }`}>
+            <div className={`{sm:relative absolute flex ml-[6rem] sm:pl-0 hover:cursor-context-menu z-10 select-none }`}>
               <div className={`{sm:pr-1 pl-3 ${!isPartitaInCoppiaRegSelected} ? ml-[-0.5rem] mr-[-2rem] : 'pl-4'}`}>
-                <span className="text-lg hover:bg-sky-900 text-pink-900/40 z-20" role="img" aria-label="" onClick={() => handleResetColors(partita.team1, partita.team2)}>
+                <span className="text-lg hover:bg-sky-900 text-gray-700/70 z-20" role="img" aria-label="" onClick={() => handleResetColors(partita.team1, partita.team2)}>
                   {/* ,,, */}
                   {/* 〰️ */}
                   ~~
