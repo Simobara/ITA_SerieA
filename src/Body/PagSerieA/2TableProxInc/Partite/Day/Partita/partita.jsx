c9d686714bf355df35086c5691ff363d0a650379
SimonBara <simobara@hotmail.it>
2024-10-16 03:43:57 +0200

    movement symbol part2

--- src/Body/PagSerieA/2TableProxInc/Partite/Day/Partita/partita.jsx
+++ src/Body/PagSerieA/2TableProxInc/Partite/Day/Partita/partita.jsx
@@ -351,7 +351,7 @@ const Partita = ({ partita, resetAll, occhioApertoPartita, setOcchioApertoPartit
   return (
     <>
       <div
-        className={`font-bold flex items-center justify-center my-[-0.1rem]
+        className={`font-bold flex items-center justify-center my-[-0.15rem]
                 ${isPartitaModificabile ? "" : "unselectable"}`}
       >
         <div className="flex items-center justify-center xs:text-xs sm:text-sm relative">
