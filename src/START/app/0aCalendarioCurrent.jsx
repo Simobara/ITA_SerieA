0ddeb555dbeff24974f93bab94d3e201bfce129e
SimonBara <simobara@hotmail.it>
2024-11-05 19:12:23 +0100

    update pron gg12

--- src/START/app/0aCalendarioCurrent.jsx
+++ src/START/app/0aCalendarioCurrent.jsx
@@ -170,19 +170,19 @@ export const calendario25 = {
   ],
     //*                                                          --- 12 ---                                                      --- 12
       giornata12: [ 
-      { numero: 1,  day: 'gio', time: '20:45',          team1: 'Genoa',        team2: 'Como',            pron: '', results: '' },
+      { numero: 1,  day: 'gio', time: '20:45',          team1: 'Genoa',        team2: 'Como',            pron: 'X', results: '' },
 
-      { numero: 2,  day: 'ven', time: '18:30',          team1: 'Venezia',      team2: 'Parma',           pron: '', results: '' },
-      { numero: 3,  day: 'ven', time: '20:45',          team1: 'Lecce',        team2: 'Empoli',          pron: '', results: '' },
+      { numero: 2,  day: 'ven', time: '18:30',          team1: 'Venezia',      team2: 'Parma',           pron: '1', results: '' },
+      { numero: 3,  day: 'ven', time: '20:45',          team1: 'Lecce',        team2: 'Empoli',          pron: '2', results: '' },
       
-      { numero: 4,  day: 'sab', time: '18:00',          team1: 'Cagliari',     team2: 'Milan',           pron: '', results: '' },
-      { numero: 5,  day: 'sab', time: '20:45',          team1: 'Juve',         team2: 'Torino',          pron: '', results: '' },
+      { numero: 4,  day: 'sab', time: '18:00',          team1: 'Cagliari',     team2: 'Milan',           pron: 'X', results: '' },
+      { numero: 5,  day: 'sab', time: '20:45',          team1: 'Juve',         team2: 'Torino',          pron: '2', results: '' },
 
-      { numero: 6,  day: 'dom', time: '12:30',          team1: 'Atalanta',     team2: 'Udinese',         pron: '', results: '' },
-      { numero: 7,  day: 'dom', time: '15:00',          team1: 'Fiorentina',   team2: 'Verona',          pron: '', results: '' },
-      { numero: 8,  day: 'dom', time: '15:00',          team1: 'Roma',         team2: 'Bologna',         pron: '', results: '' },
-      { numero: 9,  day: 'dom', time: '18:00',          team1: 'Monza',        team2: 'Lazio',           pron: '', results: '' },
-      { numero: 10, day: 'dom', time: '20:45',          team1: 'Inter',        team2: 'Napoli',          pron: '', results: '' },  
+      { numero: 6,  day: 'dom', time: '12:30',          team1: 'Atalanta',     team2: 'Udinese',         pron: 'X', results: '' },
+      { numero: 7,  day: 'dom', time: '15:00',          team1: 'Fiorentina',   team2: 'Verona',          pron: '1', results: '' },
+      { numero: 8,  day: 'dom', time: '15:00',          team1: 'Roma',         team2: 'Bologna',         pron: 'X', results: '' },
+      { numero: 9,  day: 'dom', time: '18:00',          team1: 'Monza',        team2: 'Lazio',           pron: '2', results: '' },
+      { numero: 10, day: 'dom', time: '20:45',          team1: 'Inter',        team2: 'Napoli',          pron: 'X', results: '' },  
   ],
     //*---  -----                                                                                                                --- 13
       giornata13: [
